d8ec1031774387025386837118ba0383e4077557
iwasaki.kohei <iwasaki.kohei@donuts.ne.jp>
2015-01-05 20:21:14 +0900

    fixed bug: shortenTextLength < 0

--- SwiftLabelFitToSize.swift
+++ SwiftLabelFitToSize.swift
@@ -12,9 +12,14 @@ extension UILabel {
     
     // 文字数をサイズに合わせてfitさせる
     func fitToSize() {
-        var labelSize = self.sizeThatFits(CGSizeMake(self.frame.size.width, CGFloat.max))
         
-        if labelSize.height <= self.frame.size.height {
+        var fitSize = self.sizeThatFits(CGSizeMake(self.frame.size.width, CGFloat.max))
+        var fitArea = fitSize.width * fitSize.height
+        
+        let selfArea = self.frame.size.width * self.frame.size.height
+        
+        // 既に十分サイズが大きければ終了
+        if fitArea <= selfArea {
             return
         } else {
             
@@ -22,21 +27,17 @@ extension UILabel {
             let textLength = countElements(text)
             
             // 1文字あたりのサイズを大まかに取得。
-            let labelSizeArea = labelSize.width * labelSize.height
-            let sizeForElement = labelSizeArea / CGFloat(textLength)
-
-            // 省略する文字数
-            let selfSizeArea = self.frame.size.width * self.frame.size.height
-            var shortenTextLength = Int((labelSizeArea - selfSizeArea) / sizeForElement)
+            let sizeForElement = fitArea / CGFloat(textLength)
             
-            while(labelSize.height > self.frame.size.height){
-                
+            // 省略する文字数
+            var shortenTextLength = Int((fitArea - selfArea) / sizeForElement)
+            
+            while(fitSize.height > self.frame.size.height){
                 if(textLength - shortenTextLength - 1 <= 0){ break }
-                
                 let startIndex = advance(text.startIndex, 0)
                 let endIndex = advance(startIndex, textLength - shortenTextLength - 1)
                 self.text = text[Range(start: startIndex, end: endIndex)] + "…"
-                labelSize = self.sizeThatFits(CGSizeMake(self.frame.size.width, CGFloat.max))
+                fitSize = self.sizeThatFits(CGSizeMake(self.frame.size.width, CGFloat.max))
                 shortenTextLength++
             }
         }
