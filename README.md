268847854314e53b0e7b3eb668f33df8692c6f72
iwasaki.kohei <iwasaki.kohei@donuts.ne.jp>
2015-01-06 18:00:17 +0900

    added fitToSizeByReduction

--- README.md
+++ README.md
@@ -1,29 +1,53 @@
 SwiftLabelFitToSize
 ===================
 
-Shorten and add '...' to text to fit label's size.
+SwiftLabelFitToSize provides following two extensions for UILabel.
 
-ラベルのサイズに合うようにテキストの文字数を削って「...」を追記するメソッドです。
+* **fitToSizeByTruncate** - Truncate the text to fit.
+* **fitToSizeByReduction** - Reduce the font size to fit.
 
-## Install
+SwiftLabelFitToSizeは、UILabelを拡張して以下の２つのメソッドを提供します。
+
+* **fitToSizeByTruncate** - ラベルのサイズに合うようにテキストの文字数を削ります。
+* **fitToSizeByReduction** - ラベルのサイズに合うようにフォントサイズを小さくします。
+
+## Installation
 
 Add SwiftLabelFitToSize.swift file to your project.
 
 プロジェクトにSwiftLabelFitToSize.swiftファイルを追加してください。
 
-## Example
+## FitToSizeByTruncate
 
 ```swift
+func fitToSizeByTruncate(omission:String = "…")
+```
 
+* **Description**	Truncate the text to fit.  
+* **Parameters**
+  * **omission**	String which replace the last characters with. Defaults to '…'
+
+## FitToSizeByReduction
+
+```swift
+func fitToSizeByReduction(step:CGFloat = 0.5, minimumSize:CGFloat = 5.0)  
+```
+
+* **Description**	Reduce the font size to fit.  
+* **Parameters**
+  * **step**	Step of reducing font size. Defaults to 0.5
+  * **minimumSize**	Minimum size of font size. Defaults to 5.0
+
+## Example Usage
+
+```swift
 let label = UILabel(frame: CGRectMake(0, 0, 120, 80))
 label.numberOfLines = 0
 label.text = "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"
-label.fitToSize()
-
+label.fitToSizeByTruncate()
+// same: label.fitToSizeByTruncate(omission: "…")
 ```
 
 ## Result
 
 ![](example_result.png)
-
-
--- SwiftLabelFitToSize.swift
+++ SwiftLabelFitToSize.swift
@@ -9,37 +9,71 @@
 import UIKit
 
 extension UILabel {
-    
-    // 文字数をサイズに合わせてfitさせる
-    func fitToSize() {
-        
+
+    /**
+    Truncate the text to fit.
+
+    :param: omission String which replace the last characters with. Defaults to '…'
+    */
+    func fitToSizeByTruncate(omission:String="…") {
+
         var fitSize = self.sizeThatFits(CGSizeMake(self.frame.size.width, CGFloat.max))
         var fitArea = fitSize.width * fitSize.height
-        
+
         let selfArea = self.frame.size.width * self.frame.size.height
-        
-        // 既に十分サイズが大きければ終了
+
         if fitArea <= selfArea {
             return
         } else {
-            
+
             let text = (self.text != nil) ? self.text! : ""
             let textLength = countElements(text)
-            
-            // 1文字あたりのサイズを大まかに取得。
+
+            let omissionLength = countElements(omission)
+
             let sizeForElement = fitArea / CGFloat(textLength)
-            
-            // 省略する文字数
+
             var shortenTextLength = Int((fitArea - selfArea) / sizeForElement)
-            
+
             while(fitSize.height > self.frame.size.height){
-                if(textLength - shortenTextLength - 1 <= 0){ break }
+                if(textLength - shortenTextLength - omissionLength <= 0){ break }
                 let startIndex = advance(text.startIndex, 0)
-                let endIndex = advance(startIndex, textLength - shortenTextLength - 1)
-                self.text = text[Range(start: startIndex, end: endIndex)] + "…"
+                let endIndex = advance(startIndex, textLength - shortenTextLength - omissionLength)
+                self.text = text[Range(start: startIndex, end: endIndex)] + omission
                 fitSize = self.sizeThatFits(CGSizeMake(self.frame.size.width, CGFloat.max))
                 shortenTextLength++
             }
         }
+    }
+
+    /**
+    Reduce the font size to fit.
+
+    :param: step Step of reducing font size. Defaults to 0.5
+    :param: minimumSize Minimum size of font size. Defaults to 5.0
+    */
+    func fitToSizeByReduction(step:CGFloat = 0.5, minimumSize:CGFloat = 5.0) {
+
+        var fitSize = self.sizeThatFits(CGSizeMake(self.frame.size.width, CGFloat.max))
+        var fitArea = fitSize.width * fitSize.height
+
+        let selfArea = self.frame.size.width * self.frame.size.height
+
+        let fontName = self.font.fontName
+        var fontSize = self.font.pointSize
+
+        while(fitArea > selfArea && fontSize > minimumSize) {
+
+            fontSize -= step
+            if fontSize < minimumSize {
+                fontSize = minimumSize
+            }
+            self.font = UIFont(name: fontName, size: fontSize)
+
+            fitSize = self.sizeThatFits(CGSizeMake(self.frame.size.width, CGFloat.max))
+            fitArea = fitSize.width * fitSize.height
+
+        }
+
     }
 }
